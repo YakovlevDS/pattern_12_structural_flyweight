0ee6f125df844c51704928e098cc9b96d0628d1a
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-09 11:35:39 +0200

    feat:addexample

--- flyweight.js
+++ flyweight.js
@@ -12,9 +12,21 @@ class AutoFactory {
 	create(name) {
 		let model = this.models[name];
 		if (model) return model;
+		console.count('model')
+		
 		this.models[name] = new Auto(name);
 		return this.models[name];
 	}
+	getModels(){
+		console.table(this.model)
+	}
 };
 
+const factory=new AutoFactory();
+const bmw=factory.create('BMW')
+const audi=factory.create('Audi')
+const tesla=factory.create('Tesla')
+const blackTesla=factory.create('Tesla')
+
+console.log(factory.getModels());
 
